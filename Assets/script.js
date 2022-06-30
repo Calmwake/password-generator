c53afc7887ccd1d81b8322aafdbdc6c136570be2
Justin Wilson <xjdubs@gmail.com>
2022-06-30 18:15:10 -0400

    structure

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// add listner to generate button
+// once clicked, display series of window prompts to determine password specifications
+// create function to run prompts to user
+// store each character information in it's own array
+// start prompts with password length
+// based off which criteria is chosen by the user, 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
